422f65d6d9a288bf4298e95349a37b8fd86e38f7
Nikula Daniil <daniil.nikula@gmail.com>
2022-10-01 17:24:56 +0300

    Tasks 5678 Done

--- Module 5/Program.cs
+++ Module 5/Program.cs
@@ -2,19 +2,44 @@
 {
     internal class Program
     {
-        class MainClass
+        static void Main(string[] args) 
         {
-            static void Main(string[] args) 
-            {
-            Console.WriteLine("Напишите что-то");
+            Console.WriteLine(PowerUp(2, 10));
+            Console.WriteLine(Factorial(20));
+            /*Console.WriteLine("Напишите что-то");
             var str = Console.ReadLine();
 
             Console.WriteLine("Укажите глубину эха");
             var deep = int.Parse(Console.ReadLine());
 
-            Echo(str, deep);
+            Echo(str, deep);*/
 
             Console.ReadKey();
+
+        }
+        private static int PowerUp(int N, byte pow)
+        {
+            if(pow < 1)
+            {
+                return 1;
+            }
+            else if (pow == 1)
+            {
+                return N;
+            }
+            else
+            {
+                return N * PowerUp(N, pow -= 1);
+            }
+        }
+        static decimal Factorial(int x) {
+            if (x == 0) 
+            {
+            return 1;
+            }
+            else 
+            {
+            return x * Factorial(x - 1);
             }
         }
         static void Echo(string phrase, int deep) 
